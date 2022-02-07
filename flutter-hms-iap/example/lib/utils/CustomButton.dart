8cfc4ba2de312e45b2a1e741cae0f4d1395b00e8
Bengisu Ozkul <bengisu.ozkul@huawei.com>
2022-02-07 14:18:26 +0300

    UPDATE flutter-hms-iap 6.2.0+300

---
+++ flutter-hms-iap/example/lib/utils/CustomButton.dart
@@ -0,0 +1,44 @@
+/*
+    Copyright 2020-2022. Huawei Technologies Co., Ltd. All rights reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License")
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30.0)))),
+        backgroundColor: MaterialStateProperty.all(Colors.red),
+        textStyle: MaterialStateProperty.all(TextStyle(color: Colors.white)),
+        padding: MaterialStateProperty.all(EdgeInsets.all(12.0)),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+      ),
+    );
+  }
+}
---
+++ flutter-hms-iap/ios/Classes/IapPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface IapPlugin : NSObject<FlutterPlugin>
+@end
