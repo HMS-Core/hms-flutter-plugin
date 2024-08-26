387230bd910e13d3ba3dcab5633fda9ed127c6ac
Bengisu <32999874+ozkulbeng@users.noreply.github.com>
2024-08-26 15:34:37 +0300

    UPDATE flutter-hms-mlbody 3.12.0+301

--- flutter-hms-mlbody/README.md
+++ flutter-hms-mlbody/README.md
@@ -18,16 +18,16 @@ Flutter ML Kit Body Plugin provides APIs from Huawei ML Kit SDK. The plugin cont
 - Hand Gesture Recognition
 - Liveness Detection
 
-[Learn More](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/introduction-0000001051432503?ha_source=hms1)
+[Learn More](https://developer.huawei.com/consumer/en/doc/HMS-Plugin-Guides/introduction-0000001051432503-V1?ha_source=hms1)
 
 ## Installation
 
-Please see [pub.dev](https://pub.dev/packages/huawei_ml_body/install) and [AppGallery Connect Configuration](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/config-agc-0000001164929484?ha_source=hms1).
+Please see [pub.dev](https://pub.dev/packages/huawei_ml_body/install) and [AppGallery Connect Configuration](https://developer.huawei.com/consumer/en/doc/HMS-Plugin-Guides/config-agc-0000001051911663-V1?ha_source=hms1).
 
 ## Documentation
 
-- [Quick Start](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/body-related-services-0000001074129424?ha_source=hms1)
-- [Reference](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-References/overview-0000001052975193?ha_source=hms1)
+- [Quick Start](https://developer.huawei.com/consumer/en/doc/HMS-Plugin-Guides/introduction-0000001051432503-V1?ha_source=hms1)
+- [Reference](https://developer.huawei.com/consumer/en/doc/HMS-Plugin-References/overview-0000001052975193-V1?ha_source=hms1)
 
 ## Questions or Issues
 
@@ -42,4 +42,4 @@ If you run into a bug in our samples, please submit an issue to the [GitHub repo
 
 ## License
 
-Huawei ML Body Flutter Plugin is licensed under [Apache 2.0 license](LICENSE) 
+Huawei ML Body Flutter Plugin is licensed under [Apache 2.0 license](LICENSE).
--- flutter-hms-mlbody/android/src/main/java/com/huawei/hms/flutter/mlbody/data/HMSLogger.java
+++ flutter-hms-mlbody/android/src/main/java/com/huawei/hms/flutter/mlbody/data/HMSLogger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023. Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright 2021-2024. Huawei Technologies Co., Ltd. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
@@ -37,16 +37,21 @@ public final class HMSLogger {
     private static final String TAG = "HMSLogger";
 
     private static final String KIT = "MLBody";
+
     private static final String PLATFORM = "Flutter";
-    private static final String VERSION = "3.12.0.300";
+
+    private static final String VERSION = "3.12.0.301";
 
     private static final String SERVICE = "Cross-Platform";
 
     private static final String SUCCESS = "0";
+
     private static final String UNKNOWN = "UNKNOWN";
+
     private static final String NOT_AVAILABLE = "NOT_AVAILABLE";
 
     private static final String SINGLE_EVENT_ID = "60000";
+
     private static final String PERIODIC_EVENT_ID = "60001";
 
     private static final String NETWORK_TYPE_WIFI = "WIFI";
@@ -54,17 +59,27 @@ public final class HMSLogger {
     private static volatile HMSLogger instance;
 
     private final WeakReference<Context> weakContext;
+
     private final HiAnalyticsUtils hiAnalyticsUtils;
+
     private final ConnectivityManager connectivityManager;
 
     private final Map<String, Object> singleEventMap = new HashMap<>();
+
     private final Map<String, Object> periodicEventMap = new HashMap<>();
+
     private final Map<String, Long> allCountMap = new HashMap<>();
+
     private final Map<String, Long> failCountMap = new HashMap<>();
+
     private final Map<String, Long> startTimeMap = new HashMap<>();
+
     private final Map<String, Long> firstReceiveTimeMap = new HashMap<>();
+
     private final Map<String, Long> lastReceiveTimeMap = new HashMap<>();
+
     private final Map<String, Map<String, Long>> resultCodeCountMap = new HashMap<>();
+
     private final Map<Integer, String> networkTypeMap = createNetworkTypeMap();
 
     private boolean isEnabled = false;
@@ -78,7 +93,7 @@ public final class HMSLogger {
         weakContext = new WeakReference<>(context);
         hiAnalyticsUtils = HiAnalyticsUtils.getInstance();
         connectivityManager = objectCast(context.getSystemService(Context.CONNECTIVITY_SERVICE),
-                ConnectivityManager.class);
+            ConnectivityManager.class);
 
         hiAnalyticsUtils.enableLog();
         HMSBIInitializer.getInstance(context).initBI();
@@ -151,7 +166,7 @@ public final class HMSLogger {
      * Sends unsuccessful single event
      *
      * @param methodName The name of the method called.
-     * @param errorCode  API error code
+     * @param errorCode API error code
      */
     public synchronized void sendSingleEvent(final String methodName, final String errorCode) {
         sendEvent(SINGLE_EVENT_ID, methodName, errorCode);
@@ -170,7 +185,7 @@ public final class HMSLogger {
      * Sends unsuccessful periodic event.
      *
      * @param methodName The name of the method called
-     * @param errorCode  API error code
+     * @param errorCode API error code
      */
     public synchronized void sendPeriodicEvent(final String methodName, final String errorCode) {
         sendEvent(PERIODIC_EVENT_ID, methodName, errorCode);
@@ -179,10 +194,10 @@ public final class HMSLogger {
     /**
      * Sends the event based on eventId, methodName, and resultCode.
      *
-     * @param eventId    Constant id of the event
+     * @param eventId Constant id of the event
      * @param methodName The name of the method called
      * @param resultCode Code of the method's result. "0" for success, others for
-     *                   error
+     * error
      */
     private synchronized void sendEvent(final String eventId, final String methodName, final String resultCode) {
         if (isEnabled) {
@@ -276,13 +291,13 @@ public final class HMSLogger {
     /**
      * Prepares sing-event map according to input parameters.
      *
-     * @param methodName  The name of the method called
-     * @param resultCode  Code of the method's result. "0" for success, others for
-     *                    error
+     * @param methodName The name of the method called
+     * @param resultCode Code of the method's result. "0" for success, others for
+     * error
      * @param currentTime Current timestamp in millisecond
      */
     private synchronized void putToSingleEventMap(final String methodName, final String resultCode,
-            final long currentTime) {
+        final long currentTime) {
         final long startTime = getOrDefault(startTimeMap, methodName, currentTime);
         final int costTime = (int) (currentTime - startTime);
         singleEventMap.put("apiName", methodName);
@@ -295,13 +310,13 @@ public final class HMSLogger {
     /**
      * Prepares periodic-event map according to input parameters.
      *
-     * @param methodName  The name of the method called
-     * @param resultCode  Code of the method's result. "0" for success, others for
-     *                    error
+     * @param methodName The name of the method called
+     * @param resultCode Code of the method's result. "0" for success, others for
+     * error
      * @param currentTime Current timestamp in millisecond
      */
     private synchronized void putToPeriodicEventMap(final String methodName, final String resultCode,
-            final long currentTime) {
+        final long currentTime) {
         increaseResultCodeCount(methodName, resultCode);
         increaseMapValue(methodName, allCountMap);
 
@@ -369,7 +384,7 @@ public final class HMSLogger {
      *
      * @param methodName Name of the calling method
      * @param resultCode Code of the method's result. "0" for success, others for
-     *                   error
+     * error
      */
     private synchronized void increaseResultCodeCount(final String methodName, final String resultCode) {
         final Map<String, Long> map = getOrDefault(resultCodeCountMap, methodName, new HashMap<>());
@@ -392,11 +407,11 @@ public final class HMSLogger {
      * Get the corresponding value of the key. If the key does not exist in the map
      * then the default value is returned.
      *
-     * @param map          The Map
-     * @param key          Lookup key
+     * @param map The Map
+     * @param key Lookup key
      * @param defaultValue The default value will be returned if the key is absent
-     * @param <K>          Generic type of the key
-     * @param <V>          Generic type of the value
+     * @param <K> Generic type of the key
+     * @param <V> Generic type of the value
      * @return Corresponding value or default value
      */
     private synchronized <K, V> V getOrDefault(final Map<K, V> map, final K key, final V defaultValue) {
@@ -406,11 +421,11 @@ public final class HMSLogger {
     /**
      * Put key-value pair to map if the key is absent.
      *
-     * @param map   The Map
-     * @param key   Lookup key
+     * @param map The Map
+     * @param key Lookup key
      * @param value The value will be put to the map if the key is absent
-     * @param <K>   Generic type of the key
-     * @param <V>   Generic type of the value
+     * @param <K> Generic type of the key
+     * @param <V> Generic type of the value
      */
     private synchronized <K, V> void putIfAbsent(final Map<K, V> map, final K key, final V value) {
         if (!map.containsKey(key)) {
@@ -422,9 +437,9 @@ public final class HMSLogger {
      * Utility method that castes given object to given class type.
      *
      * @param source Source object to be casted
-     * @param clazz  Class that object will be casted to its type
-     * @param <S>    Source object's type
-     * @param <D>    Destination type
+     * @param clazz Class that object will be casted to its type
+     * @param <S> Source object's type
+     * @param <D> Destination type
      * @return Object that casted to D type
      */
     private synchronized <S, D> D objectCast(final S source, final Class<D> clazz) {
